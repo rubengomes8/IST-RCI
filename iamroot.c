de58c720ad64b65d42bc0b523bf01507395ecdfb
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2019-03-03 13:28:23 +0000

    Criei a função arguments_reading

--- iamroot.c
+++ iamroot.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]){
 		int n = 0, i = 0, has_stream = 0;
 		int flag_b = 1, flag_d = 0, flag_h = 0;
 		char buffer[BUFFER_SIZE];
+		char *token;
 
 		//Só para ver se há stream ou não
 		if(argc > 1)
@@ -43,69 +44,7 @@ int main(int argc, char *argv[]){
 
 		//Se houver stream começa a ler do argv[3], se não do argv[2]
 		//Acho que isto dá problema se executarmos com uma das flags isoladas (b, d ou h) antes de uma das outras.
-		for (n = 1; n < argc - 1; n+=2)
-    {
-				//ler o resto da informação com as tags correspondentes
-				if(!strcmp(argv[n + has_stream], "-i")){
-							strcpy(ipaddr, argv[n + has_stream + 1]);
-							printf("ipaddr: %s\n", ipaddr);
-				}	else if(!strcmp(argv[n + has_stream], "-t")){
-							strcpy(tport, argv[n + has_stream + 1]);
-							printf("tport: %s\n", tport);
-				} else if(!strcmp(argv[n + has_stream], "-u")){
-							strcpy(uport, argv[n + has_stream + 1]);
-							printf("uport: %s\n", uport);
-				}	else if(!strcmp(argv[n + has_stream], "-s")){
-							i = count_specific_char(argv[n + has_stream + 1], ':');
-							if(i == 2){
-									//split into rsaddr e rsport
-									//rsaddr = strtok(argv[n + has_stream + 1], ":");
-									//rsport = strtok(NULL, " \n");
-									//printf("rsaddr: %s\n", rsaddr);
-									//printf("rsaddr: %s\n", rsport);
-
-							} else if (i == 1){
-									//strcpy(rsadrr, argv[n + has_stream + 1]);
-							}
-				} else if(!strcmp(argv[n + has_stream], "-p")){
-							tcp_sessions = atoi(argv[n + has_stream + 1]);
-							if(tcp_sessions >= 1)
-									break;
-							else{
-									printf("tcpsessions must be at least 1!\n");
-									exit(-1);
-							}
-				} else if(!strcmp(argv[n + has_stream], "-n")){
-							bestpops = atoi(argv[n + has_stream + 1]);
-							if(bestpops >= 1)
-									break;
-							else{
-									printf("bestpops must be at least 1!\n");
-									exit(-1);
-							}
-				} else if(!strcmp(argv[n + has_stream], "-x")){
-							tsecs = atoi(argv[n + has_stream + 1]);
-							if(tsecs >= 1) //Estou a exigir que o período seja pelo menos 1 segundo mas podemos mudar este valor
-									break;
-							else{
-									printf("tsecs must be at least 1!\n");
-									exit(-1);
-							}
-				} else if(!strcmp(argv[n + has_stream], "-b")){
-							flag_b = 0;
-							n--;
-				} else if(!strcmp(argv[n + has_stream], "-d")){
-							flag_d = 1;
-							n--;
-				} else if(!strcmp(argv[n + has_stream], "-h")){
-							flag_h = 1;
-							n--;
-				} else{
-							printf("Usage of incorrect flags!\n"); // talvez imprimir o metodo e invocação
-							exit(-1);
-				}
-
-    }
+		arguments_reading(argc, argv, has_stream, ipaddr, tport, uport, rsaddr, rsport, &tcp_sessions, &bestpops, &tsecs, &flag_b, &flag_d, &flag_h);
 
 		printf("\n%d\n", argc);
 
--- utils.h
+++ utils.h
@@ -7,5 +7,7 @@
 
 
 int count_specific_char(char *string, char ch);
+void arguments_reading(int argc, char *argv[], int has_stream, char ipaddr[], char tport[], char uport[], char rsaddr[], char rsport[], int *tcp_sessions, int *bestpops, int *tsecs, int *flag_b, int *flag_d, int *flag_h);
+
 
 #endif //UTILS_UTILS_H
